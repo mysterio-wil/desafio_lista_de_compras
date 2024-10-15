e59b522e5912692620b81bd2f0fc2825f6272419
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-14 20:15:59 -0500

    Se añadió implements Comparable<> a la clase Compra, se implementó el método compareTo en la clase compra

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,4 @@
-public class Compra {
+public class Compra implements Comparable<Compra>{
     private double valor;
     private String descripcion;
 
@@ -20,5 +20,10 @@ public class Compra {
     public String toString() {
         return "Compra: valor=" + valor +
                 ", descripcion='" + descripcion;
+    }
+
+    @Override
+    public int compareTo(Compra otraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
     }
 }
