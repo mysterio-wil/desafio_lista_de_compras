2a27ce7238be8590a475d5793a913d2cf5535ecb
Wilmer Gulcochia <karlwgs1989@gmail.com>
2024-10-14 19:50:47 -0500

    Se creó y se implementó un método booleano lanzarCompra(compra)

--- src/TarjetaDeCredito.java
+++ src/TarjetaDeCredito.java
@@ -12,6 +12,15 @@ public class TarjetaDeCredito {
         this.listaDeCompras = new ArrayList<>();
     }
 
+    public boolean lanzarCompra(Compra compra) {
+        if(this.saldo >= compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.listaDeCompras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
     public double getLimite() {
         return limite;
     }
